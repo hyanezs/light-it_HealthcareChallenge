72c02f78fadf4bd49f0be747dbdaf501ee83aeb9
Hernan Yáñez <heryanez212@gmail.com>
2023-06-25 18:56:48 -0300

    removed /me delay

--- backend/src/middlewares/devDelay.ts
+++ backend/src/middlewares/devDelay.ts
@@ -1,7 +1,12 @@
 import { type NextFunction, type Request, type Response } from 'express';
 import { logger } from '../utils';
 
-export default async (_req: Request, _res: Response, next: NextFunction) => {
+export default async (req: Request, _res: Response, next: NextFunction) => {
+  if (req.url === '/me') {
+    next();
+    return;
+  }
+
   logger.debug(
     'Randomly delaying response in DEV to simulate api request time...',
   );
